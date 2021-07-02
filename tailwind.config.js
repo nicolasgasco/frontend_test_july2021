fd564f497395125b2d4447eac36d6451899e06f8
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 23:17:50 +0200

    Added custom colors preserving default ones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,28 @@
+const { colors: defaultColors } = require("tailwindcss/defaultTheme");
+
+const colors = {
+  ...defaultColors,
+  ...{
+    primary: {
+      DEFAULT: "#333",
+      light: "#E9E9E9",
+    },
+    secondary: {
+      DEFAULT: "#5596DF",
+      alt: "96C7FF",
+    },
+    product: {
+      pink: "#F5E5DA",
+      gray: "E1E1D7",
+    },
+  },
+};
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: {
-        DEFAULT: "#333",
-        light: "#E9E9E9"
-      },
-      secondary: {
-        DEFAULT: "#5596DF",
-        alt: "96C7FF",
-      },
-      product: {
-        pink: "#F5E5DA",
-        gray: "E1E1D7",
-      },
-    },
+    colors: colors,
     extend: {},
   },
   variants: {
