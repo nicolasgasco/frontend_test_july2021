1008c3cc7702eb421a7cb4bc919ffdbec8f2bbeb
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 22:49:30 +0200

    Added component with background picture of parrot

---
+++ src/components/UI/PictureBox.js
@@ -0,0 +1,11 @@
+import classes from "./PictureBox.module.css";
+
+const PictureBox = () => {
+  return (
+    <section className={`h-96 ${classes.box}`}>
+      <p>Ciao</p>
+    </section>
+  );
+};
+
+export default PictureBox;
