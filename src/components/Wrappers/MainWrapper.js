31185c11afa250c87ab41e3563c281e752495c6f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 22:50:23 +0200

    Added style for main wrapper

--- src/components/Wrappers/MainWrapper.js
+++ src/components/Wrappers/MainWrapper.js
@@ -1,11 +1,15 @@
 // Wrapper created to give central part of page an even look
+import classes from "./MainWrapper.module.css";
 
-const MainWrapper = ({children, className}) => {
-    return (
-        <main className={` ${className}`}>
-            {children}
-        </main>
-    )
-}
+const MainWrapper = ({ children, className }) => {
+  return (
+    <main
+      id="main-wrapper"
+      className={`flex-1 w-full mx-auto outline-black ${classes.wrapper} ${className}`}
+    >
+      {children}
+    </main>
+  );
+};
 
 export default MainWrapper;
