bcd2d7c3228fbd0e356e7f846ffdc327cd0b60aa
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 23:17:58 +0200

    Added title

--- src/components/UI/LogoDiv.js
+++ src/components/UI/LogoDiv.js
@@ -3,7 +3,12 @@ import Logo from "../../assets/logo/Logotipo.svg";
 
 const LogoDiv = ({ className }) => {
   return (
-    <img src={Logo} alt="Lorem logo" className={`w-24 ml-20 ${className}`} />
+    <img
+      src={Logo}
+      title="Lorem"
+      alt="Lorem logo"
+      className={`w-24 ml-20 ${className}`}
+    />
   );
 };
 
