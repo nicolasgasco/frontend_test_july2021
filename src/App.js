281c1d93ac8944173a139c8a5bd7f4fd68a684f9
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 22:49:52 +0200

    Added MainPage content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import MainWrapper from "./components/Wrappers/MainWrapper";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <MainWrapper>
+            <MainPage />
+          </MainWrapper>
+        </Route>
+
+        {/* Catch all for wrong routes */}
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
