261b7186267dd2cadba265a497acc123d556b39e
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 23:18:05 +0200

    Added h1 tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,11 @@ const Header = () => {
   return (
     <header className="bg-primary h-20 flex justify-between items-center">
       <nav>
-        <Link to="/">
-          <LogoDiv />
-        </Link>
+        <h1>
+          <Link to="/">
+            <LogoDiv />
+          </Link>
+        </h1>
       </nav>
     </header>
   );
