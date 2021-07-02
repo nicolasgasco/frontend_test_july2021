b6da31a3071b9e76578ba7224948a6923651b908
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 23:17:36 +0200

    Added text

--- src/components/UI/PictureBox.js
+++ src/components/UI/PictureBox.js
@@ -3,7 +3,10 @@ import classes from "./PictureBox.module.css";
 const PictureBox = () => {
   return (
     <section className={`h-96 ${classes.box}`}>
-      <p>Ciao</p>
+      <div className="w-60 outline-white">
+        <h2 className="text-3xl font-bold text-white">Lorem ipsum</h2>
+        <p className="text-2xl break-all text-white">Dolor sit amet consectetur adipiscing elit.</p>
+      </div>
     </section>
   );
 };
