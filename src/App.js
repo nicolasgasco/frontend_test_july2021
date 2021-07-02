79a8d2317140c00bbc1ae6c2b738f08dace25a58
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-02 21:44:12 +0200

    CSS file not needed anymore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import "./App.css";
-
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 
