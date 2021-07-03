ac11c3c5c9e3cd6685e494de3ef1b29cf54034da
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 23:13:49 +0200

    Finished mobile styles

--- src/components/Wrappers/FullWidthWrapper.js
+++ src/components/Wrappers/FullWidthWrapper.js
@@ -1,7 +1,7 @@
 const FullWidthWrapper = ({ children, className }) => {
   return (
-    <div className={`bg-primary min-w-full py-3 `}>
-      <div className={`px-5 sm:px-0 max-w-1240 mx-auto sm:h-24 ${className}`}>
+    <div className={`bg-primary min-w-full py-3 sm:py-0 sticky top-0 sm:static z-10`}>
+      <div className={`px-5 sm:px-0 max-w-1240 mx-auto sm:block sm:h-24 ${className}`}>
         {children}
       </div>
     </div>
