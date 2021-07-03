4004c5c8b21fbdebf5db111d375ef70eaad7ac46
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 15:09:25 +0200

    Added products and signin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import MainWrapper from "./components/Wrappers/MainWrapper";
 import MainPage from "./pages/MainPage";
+import ProductsPage from "./pages/ProductsPage";
+import SigninPage from "./pages/SigninPage";
 
 function App() {
   return (
@@ -13,6 +15,18 @@ function App() {
         <Route exact path="/">
           <MainWrapper>
             <MainPage />
+          </MainWrapper>
+        </Route>
+
+        <Route exact path="/products">
+          <MainWrapper>
+            <ProductsPage />
+          </MainWrapper>
+        </Route>
+
+        <Route exact path="/auth/signin">
+          <MainWrapper>
+            <SigninPage />
           </MainWrapper>
         </Route>
 
