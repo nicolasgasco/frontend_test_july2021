e1c3f08d34b090e6f1f7b2a4fce56e46ce551436
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 23:13:31 +0200

    Tweaked style

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,3 +1,4 @@
+import MenuOverlay from "../components/Menu/MenuOverlay";
 import ProductCard from "../components/Products/ProductCard";
 import PictureBox from "../components/UI/PictureBox";
 
@@ -6,7 +7,9 @@ const MainPage = () => {
     <>
       <PictureBox />
       <section className="mt-12 px-6 sm:px-0">
-        <h3 className="h3 text-primary-darkest tracking-tight mb-2 sm:mb-6">Featured products</h3>
+        <h3 className="h3 text-primary-darkest tracking-tight mb-2 sm:mb-6">
+          Featured products
+        </h3>
         <div className="flex flex-col sm:flex-row flex-wrap justify-start align-middle mb-8 sm:mb-20 divide-y-2 sm:divide-y-0">
           <ProductCard />
           <ProductCard />
