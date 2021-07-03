67e5eb0ddd33c2f34b9b87062da4db09a994d32d
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 15:12:44 +0200

    Added content

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,18 @@
+import ProductCard from "../components/Products/ProductCard";
 import PictureBox from "../components/UI/PictureBox";
 
 const MainPage = () => {
   return (
-    <div className="outline-black">
+    <>
       <PictureBox />
-    </div>
+      <section className="mt-12">
+        <h3 className="h3 tracking-tight mb-6">Featured products</h3>
+        <div className="flex justify-start align-middle">
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </section>
+    </>
   );
 };
 
