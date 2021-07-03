73c3570ca226115f44292d5fa7a7a827182cbc1f
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 23:14:59 +0200

    Created new component for signin form

---
+++ src/components/Auth/SigninForm.js
@@ -0,0 +1,48 @@
+const SigninForm = () => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="mt-10" onSubmit={handleFormSubmit}>
+      <div className="mb-8">
+        <label
+          for="username"
+          className="text-md font-bold text-primary-darkest"
+        >
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          autoComplete="username"
+          className="w-full ring-1 ring-primary-darkest h-12 px-3 tracking-widest"
+        />
+      </div>
+      <div className="mb-8">
+        <label
+          for="username"
+          className="text-md font-bold text-primary-darkest"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          autoComplete="current-password"
+          className="w-full ring-1 ring-primary-darkest h-12 px-3 tracking-widest"
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-secondary text-white font-bold rounded-md h-14 hover:bg-secondary-light"
+      >
+        Sign in
+      </button>
+    </form>
+  );
+};
+
+export default SigninForm;
