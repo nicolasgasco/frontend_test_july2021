24d3bd3b173f817d6385558f59215a11dea1a644
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-03 15:12:13 +0200

    Added custom max width

--- src/components/Wrappers/MainWrapper.js
+++ src/components/Wrappers/MainWrapper.js
@@ -1,11 +1,9 @@
 // Wrapper created to give central part of page an even look
-import classes from "./MainWrapper.module.css";
-
 const MainWrapper = ({ children, className }) => {
   return (
     <main
       id="main-wrapper"
-      className={`flex-1 w-full mx-auto outline-black ${classes.wrapper} ${className}`}
+      className={`flex-1 w-full mx-auto max-w-1240 ${className}`}
     >
       {children}
     </main>
