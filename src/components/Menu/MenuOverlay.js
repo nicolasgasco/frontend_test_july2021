38de82f0083b521c75cd655e1651cb9c21b90993
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-04 10:00:43 +0200

    Added keys and routes

--- src/components/Menu/MenuOverlay.js
+++ src/components/Menu/MenuOverlay.js
@@ -7,16 +7,21 @@ const MenuOverlay = ({ className, setMobileOpen }) => {
   const menuItems = ["Products", "About us", "Contact"];
 
   const showMenuItems = menuItems.map((item) => {
+    // To extract the route automatically, e.g. About us -> about
+    const firstWordLowercase = item.split(" ")[0].toLowerCase();
+    
     return (
-      <Link to="#" className="text-4xl font-bold text-white py-12">{`${
-        item.charAt(0).toUpperCase() + item.slice(1)
-      }`}</Link>
+      <Link
+        key={`menu-item-${firstWordLowercase}`}
+        to={firstWordLowercase}
+        className="text-4xl font-bold text-white py-12"
+      >{`${item.charAt(0).toUpperCase() + item.slice(1)}`}</Link>
     );
   });
 
   return (
     <div
-      className={`sm:hidden fixed top-12 w-screen h-full z-50 bg-primary-darkest bg-opacity-95 ${className} ${classes.dropdown} `}
+      className={`sm:hidden fixed top-12 w-screen h-full z-50 bg-primary-darkest bg-opacity-95 ${className}`}
     >
       <nav className="w-full flex flex-col items-center mt-12">
         {showMenuItems}
