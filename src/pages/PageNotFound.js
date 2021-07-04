d1e524133334890c786ab7a2c77de0ddfdde5cc3
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-04 10:00:02 +0200

    Added page for non existing pages

---
+++ src/pages/PageNotFound.js
@@ -0,0 +1,21 @@
+import { ArrowLeft } from "heroicons-react";
+import { useHistory } from "react-router-dom";
+
+const ProductsPage = () => {
+  const history = useHistory();
+
+  return (
+    <section className="p-10">
+      <button
+        onClick={() => history.goBack()}
+        class="text-primary-darkest flex items-center mb-16"
+      >
+        <ArrowLeft aria-hidden="true" className="inline mr-3 w-5" />
+        <span className="font-bold text-lg tracking-tight">Go back</span>
+      </button>
+      <h2 className="mt-8 h2">Page under construction</h2>
+    </section>
+  );
+};
+
+export default ProductsPage;
--- src/pages/ProductsPage.js
+++
@@ -1,9 +0,0 @@
-const ProductsPage = () => {
-  return (
-    <section className="p-10">
-      <h2 className="mt-8 h2">This page doesn't exist yet :(</h2>
-    </section>
-  );
-};
-
-export default ProductsPage;
