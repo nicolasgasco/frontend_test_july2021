6a4fbf8c270c1fdc277297c17a5c557fb6fd49a6
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-06 15:44:09 +0200

    Removed part of redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import menuReducer from "./menu";
-import authReducer from "./auth";
-import productsReducer from "./products";
 
 const store = configureStore({
   reducer: {
     menu: menuReducer,
-    auth: authReducer,
-    products: productsReducer,
   },
 });
 
