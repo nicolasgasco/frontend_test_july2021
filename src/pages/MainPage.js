2273d0ef2a38f4d1b0cb93ac8fa951d744bb3830
nicolasgasco <nicolas.gasco92@gmail.com>
2021-07-06 15:44:44 +0200

    Added products

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,22 @@
-import MenuOverlay from "../components/Menu/MenuOverlay";
+import { useEffect, useState } from "react";
+
 import ProductCard from "../components/Products/ProductCard";
 import PictureBox from "../components/UI/PictureBox";
 
+import { PRODUCTS } from "../assets/mock_db/products";
+
 const MainPage = () => {
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+
+  // This could could be async in real life, hence UseEffect
+  useEffect(() => {
+    setFeaturedProducts(PRODUCTS);
+  }, []);
+
+  const showFeaturedProducts = featuredProducts.map((product) => {
+    return <ProductCard key={product._id} product={product} />;
+  });
+
   return (
     <>
       <PictureBox />
@@ -11,8 +25,7 @@ const MainPage = () => {
           Featured products
         </h3>
         <div className="flex flex-col sm:flex-row flex-wrap justify-start align-middle mb-8 sm:mb-20 divide-y-2 sm:divide-y-0">
-          <ProductCard />
-          <ProductCard />
+          {showFeaturedProducts}
         </div>
       </section>
     </>
